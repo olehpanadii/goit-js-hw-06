ddeaabac335ac891824bd07eec37f666b8e39dcb
OlegPanadiy <opanadiy@gmail.com>
2023-07-30 15:32:23 +0100

    HW 6

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handlarSubmit);
+
+function handlarSubmit(evt) {
+  evt.preventDefault();
+  const { email, password } = evt.currentTarget.elements;
+
+  if (email.value === "" || password.value === "") {
+    alert(`Всі поля повинні бути заповненими`);
+  } else {
+    console.log({ Email: email.value, Password: password.value });
+    form.reset();
+  }
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const elements = {
+  button: document.querySelector(".change-color"),
+  colorSpan: document.querySelector(".color"),
+  body: document.querySelector("body"),
+};
+elements.button.addEventListener("click", onClick);
+
+function onClick() {
+  elements.body.style.backgroundColor = getRandomHexColor();
+  elements.colorSpan.textContent = getRandomHexColor();
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
